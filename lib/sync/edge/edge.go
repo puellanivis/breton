b93afab03b735d4634bf5352628ca42295d2fc3d
Cassondra Foesch <puellanivis@gmail.com>
2017-06-26 14:20:35 +0000

    lib/sync/edge: go fmt; golint; go vet

--- lib/sync/edge/edge.go
+++ lib/sync/edge/edge.go
@@ -4,8 +4,10 @@ import (
 	"sync/atomic"
 )
 
+// Edge defines a synchronization object that deifnes two states Up and Down
 type Edge int64
 
+// String returns a momentary state of the Edge.
 func (e *Edge) String() string {
 	v := atomic.LoadInt64((*int64)(e))
 	if v == 0 {
@@ -15,10 +17,12 @@ func (e *Edge) String() string {
 	return "up"
 }
 
+// Up will ensure that the Edge is in state Up, and returns true only if state changed.
 func (e *Edge) Up() bool {
 	return atomic.CompareAndSwapInt64((*int64)(e), 0, 1)
 }
 
+// Down will ensure that the Edge is in state Down, and returns true only if state changed.
 func (e *Edge) Down() bool {
 	return atomic.CompareAndSwapInt64((*int64)(e), 1, 0)
 }
