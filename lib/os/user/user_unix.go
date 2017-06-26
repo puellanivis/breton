3418f7263a9ca4464a8e34744d192fb6b9794c20
Cassondra Foesch <puellanivis@gmail.com>
2017-06-26 11:24:31 +0000

    lib/os/user: implements a fix to os/user that prevents GOOS=windows from polling the active domain server first before a local cache. This resulted in _long_ start up times on my network.

---
+++ lib/os/user/user_unix.go
@@ -0,0 +1,38 @@
+// Wraps the golang standard library, unless it's Windows, then use NetLookup instead of DomainLookup first.
+
+// +build !windows
+
+package user
+
+import (
+	"os/user"
+)
+
+type Group struct{user.Group}
+type User struct{user.User}
+
+var (
+	LookupGroup = user.LookupGroup
+	LookupGroupId = user.LookupGroupId
+	Current = user.Current
+	Lookup = user.Lookup
+	LookupId = user.LookupId
+)
+
+func CurrentHomeDir() (string, error) {
+	me, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+
+	return me.HomeDir, nil
+}
+
+func CurrentUsername() (string, error) {
+	me, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+
+	return me.Username, nil
+}
