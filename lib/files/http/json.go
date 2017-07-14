51cbee8e151647a31b7e0e457dff536a14b28c21
Cassondra Foesch <puellanivis@gmail.com>
2017-07-14 21:35:05 +0000

    lib/files/http: add a convenience function REST(ctx, uri, input, output) error

--- lib/files/http/json.go
+++ lib/files/http/json.go
@@ -1,10 +1,21 @@
 package http
 
 import (
+	"context"
 	"encoding/json"
 
 	"lib/files"
 )
+
+// REST is a convenience function, that marshals the send parameter into JSON, uses it as an attached content to read the uri, and then unmarshals the JSON received into the recv parameter.
+func REST(ctx context.Context, uri string, send, recv interface{}) error {
+	r, err := files.Open(ctx, uri, WithJSON(send))
+	if err != nil {
+		return err
+	}
+
+	return files.ReadJSONFrom(r, recv)
+}
 
 // WithJSON takes a value which is marshalled to JSON, and then attached to a Context, which is then used as the POST body in this library.
 func WithJSON(v interface{}) files.Option {
