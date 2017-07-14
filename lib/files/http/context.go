777644e77ac68317e8a834ce3e84c2a49d971a70
Cassondra Foesch <puellanivis@gmail.com>
2017-07-14 21:11:07 +0000

    lib/files/http: add WithUserAgent to context wrapping, as this should be universal for a whole context, it is much more appropriate for a context

--- lib/files/http/context.go
+++ lib/files/http/context.go
@@ -10,6 +10,7 @@ type key int
 
 const (
 	clientKey key = iota
+	userAgentKey
 )
 
 // WithClient attaches an http.Client to the Context, that will be used by this library as the http.Client
@@ -20,4 +21,14 @@ func WithClient(ctx context.Context, cl *http.Client) context.Context {
 func getClient(ctx context.Context) (*http.Client, bool) {
 	cl, ok := ctx.Value(clientKey).(*http.Client)
 	return cl, ok
+}
+
+// WithUserAgent attaches an string to the Context, that will be used by this library as the User-Agent in all headers
+func WithUserAgent(ctx context.Context, ua string) context.Context {
+	return context.WithValue(ctx, userAgentKey, ua)
+}
+
+func getUserAgent(ctx context.Context) (string, bool) {
+	ua, ok := ctx.Value(userAgentKey).(string)
+	return ua, ok
 }
