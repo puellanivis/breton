a4a50d585fedd7e20816e08abf9a3639e31082ac
Cassondra Foesch <puellanivis@gmail.com>
2017-07-16 13:42:38 +0000

    lib/flag: golint

--- lib/flag/option.go
+++ lib/flag/option.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 )
 
+// Option is a function that sets a specified function upon a flag.Flag.
+// It returns an Option that will revert the option set.
 type Option func(*Flag) Option
 
+// WithShort returns an Option that will set the Short Flag of a flag to the
+// specified rune.
 func WithShort(shortFlag rune) Option {
 	return func(f *Flag) Option {
 		save := f.Short
@@ -15,6 +19,11 @@ func WithShort(shortFlag rune) Option {
 	}
 }
 
+// WithDefault returns an Option that will set the default value of a flag
+// to the value given. If when setting the value, the values cannot be assigned
+// to the flag, then it will panic. (This should mostly be during initilization
+// so while it cannot be checked at compile-time, it should result in an
+// immediate panic when running the program.)
 func WithDefault(value interface{}) Option {
 	return func(f *Flag) Option {
 		save := f.Value.Get()
