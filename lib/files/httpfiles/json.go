0aea76e04bdeead2a35b28e7b73971edbdf3e1c6
Cassondra Foesch <puellanivis@gmail.com>
2017-07-21 16:23:28 +0000

    lib/files/httpfiles: use lib/files/json.Marshal with lib/files/json.Option rather than a separate Indent function

--- lib/files/httpfiles/json.go
+++ lib/files/httpfiles/json.go
@@ -2,9 +2,9 @@ package httpfiles
 
 import (
 	"context"
-	"encoding/json"
 
 	"github.com/puellanivis/breton/lib/files"
+	"github.com/puellanivis/breton/lib/files/json"
 )
 
 // REST is a convenience function, that marshals the send parameter into JSON, uses it as an attached content to read the uri, and then unmarshals the JSON received into the recv parameter.
@@ -14,25 +14,13 @@ func REST(ctx context.Context, uri string, send, recv interface{}) error {
 		return err
 	}
 
-	return files.ReadJSONFrom(r, recv)
+	return json.ReadFrom(r, recv)
 }
 
 // WithJSON takes a value which is marshalled to JSON, and then attached to a Context, which is then used as the POST body in this library.
-func WithJSON(v interface{}) files.Option {
+func WithJSON(v interface{}, opts ...json.Option) files.Option {
 	return func(f files.File) (files.Option, error) {
-		b, err := json.Marshal(v)
-		if err != nil {
-			return nil, err
-		}
-
-		return WithContent("POST", "application/json", b)(f)
-	}
-}
-
-// WithJSONIndent takes a value which is marshalled to JSON with indentions, and then attached to a Context, which is then used as the POST body in this library. (Sometimes, some RPC servers do not like non-indented JSON.)
-func WithJSONIndent(v interface{}) files.Option {
-	return func(f files.File) (files.Option, error) {
-		b, err := json.MarshalIndent(v, "", "  ")
+		b, err := json.Marshal(v, opts...)
 		if err != nil {
 			return nil, err
 		}
