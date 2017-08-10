5aae47d429bb39c9207a19d893efd544a5ba3b35
Cassondra Foesch <puellanivis@gmail.com>
2017-08-10 15:38:22 +0200

    lib/gnuflag: change package-level godoc to reference correct package name.

--- lib/gnuflag/flag.go
+++ lib/gnuflag/flag.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-/*	Package flag implements command-line flag parsing compatible with GNU longopts.
+/*	Package gnuflag implements command-line flag parsing compatible with GNU longopts.
 
 	Usage:
 
