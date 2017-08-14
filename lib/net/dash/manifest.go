fd5dc8309c628a16be99509cf7a212f45ef0f9be
Cassondra Foesch <puellanivis@gmail.com>
2017-08-14 11:50:17 +0200

    lib/net/dash: alter Manifest to support changes

--- lib/net/dash/manifest.go
+++ lib/net/dash/manifest.go
@@ -86,11 +86,10 @@ func New(ctx context.Context, manifest string) (*Manifest, error) {
 		}
 	}
 
-	// the xs.duration type has a minimum resolution of PT1S (1 second)
-	// it’s possible they could implement PT1m, or PT1n, but not now…
 	minTime := m.MinimumUpdatePeriod.Duration
-	if minTime < 1*time.Second {
-		minTime = 1 * time.Second
+	// I can’t imagine using a minimum time of less than one millisecond.
+	if minTime < time.Millisecond {
+		minTime = time.Millisecond
 	}
 
 	return &Manifest{
@@ -110,51 +109,10 @@ func (m *Manifest) MinimumUpdatePeriod() time.Duration {
 	return m.m.expTime
 }
 
-// Pull reads the next series of segments in the Stream. If time.Duration
-// is 0, it means that no new segments were available. (You’re likely calling
-// this function too often in this case. Calling any faster than the duration
-// returned by MinimumUpdatePeriod is just a waste of cycles.)
-func (m *Manifest) Pull(ctx context.Context, s *Stream) (time.Duration, error) {
-	ctx, err := files.WithRoot(ctx, m.base)
-	if err != nil {
-		return 0, err
-	}
-
-	cur, err := m.m.get(ctx)
-	if err != nil {
-		return 0, err
-	}
-
-	for _, p := range cur.Period {
-		if p.Id != s.pid {
-			continue
-		}
-
-		for _, as := range p.AdaptationSet {
-			if as.Id != s.aid {
-				continue
-			}
-
-			tmpl := as.SegmentTemplate
-			if tmpl == nil {
-				return 0, errors.New("SegmentTemplate not found")
-			}
-
-			if tmpl.SegmentTimeline == nil {
-				return 0, errors.New("SegmentTimeline not found")
-			}
-
-			return s.readTimeline(ctx, tmpl.Timescale, tmpl.StartNumber, tmpl.SegmentTimeline)
-		}
-	}
-
-	return 0, fmt.Errorf("no media could be found for pid: %s, aid: %d", s.pid, s.aid)
-}
-
 // Stream returns a Stream object that specifies a specific series of segments within the DASH manifest.
-func (m *Manifest) Stream(w io.Writer, mimeType string, picker PickRepFunc) (*Stream, error) {
+func (m *Manifest) Stream(w io.Writer, mimeType string, picker Picker) (*Stream, error) {
 	if picker == nil {
-		picker = PickFirst
+		picker = PickFirst()
 	}
 
 	as, ok := m.adaptations[mimeType]
@@ -162,7 +120,7 @@ func (m *Manifest) Stream(w io.Writer, mimeType string, picker PickRepFunc) (*St
 		return nil, errors.New("mime-type not available")
 	}
 
-	var best *mpd.Representation
+	var picked *mpd.Representation
 
 	for _, rep := range as.reps {
 		// I don’t know how this could end up being here,
@@ -171,24 +129,23 @@ func (m *Manifest) Stream(w io.Writer, mimeType string, picker PickRepFunc) (*St
 			continue
 		}
 
-		if picker(best, rep) {
-			best = rep
-		}
+		picked = picker(rep)
 	}
 
-	if best == nil {
+	if picked == nil {
 		return nil, errors.New("no suitable representations found")
 	}
 
 	if glog.V(1) {
-		glog.Infof("chose %s with bandwidth: %d", mimeType, best.Bandwidth)
-		if best.Height > 0 && best.Width > 0 {
-			glog.Infof("chose %s with %d×%d", mimeType, best.Height, best.Width)
+		glog.Infof("chose %s with bandwidth: %d", mimeType, picked.Bandwidth)
+		if picked.Height > 0 && picked.Width > 0 {
+			glog.Infof("chose %s with %d×%d", mimeType, picked.Height, picked.Width)
 		}
 	}
 
 	return &Stream{
 		w: w,
+		m: m,
 
 		metrics: m.metrics.WithLabels(typeLabel.WithValue(mimeType)),
 
@@ -199,7 +156,7 @@ func (m *Manifest) Stream(w io.Writer, mimeType string, picker PickRepFunc) (*St
 		init:    as.init,
 		media:   as.media,
 
-		Bandwidth: best.Bandwidth,
-		RepID:     string(best.Id),
+		bw:    picked.Bandwidth,
+		repID: string(picked.Id),
 	}, nil
 }
