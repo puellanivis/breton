03bf782a3dfbd2c50003dcb2b96cbbb6df708209
Cassondra Foesch <puellanivis@gmail.com>
2017-09-05 12:21:29 +0000

    lib/gnuflag: change NewFunc to give a default string, instead of a name

--- lib/gnuflag/funcs.go
+++ lib/gnuflag/funcs.go
@@ -7,24 +7,24 @@ type SetterFunc func(string) error
 
 // FuncValue describes a flag which will call a func(string) error when specified as a flag.
 type FuncValue struct {
-	name   string
+	value  string
 	isBool bool
 	f      SetterFunc
 }
 
 // NewFunc returns a FuncValue that acts as a boolean flag.
-func NewFunc(name string, fn func()) *FuncValue {
+func NewFunc(val string, fn func()) *FuncValue {
 	return &FuncValue{
-		name:   name,
+		value:  val,
 		isBool: true,
 		f:      func(s string) error { fn(); return nil },
 	}
 }
 
 // NewFuncWithArg returns a FuncValue that acts as a normal flag.
-func NewFuncWithArg(name string, fn SetterFunc) *FuncValue {
+func NewFuncWithArg(val string, fn SetterFunc) *FuncValue {
 	return &FuncValue{
-		name:   name,
+		value:  val,
 		isBool: false,
 		f:      fn,
 	}
@@ -32,7 +32,7 @@ func NewFuncWithArg(name string, fn SetterFunc) *FuncValue {
 
 // String returns a String representation of this flag. (TODO: should be something other than empty string.
 func (f *FuncValue) String() string {
-	return ""
+	return f.value
 }
 
 // Set calls the function of the FuncValue and returns its error.
@@ -62,14 +62,14 @@ func FuncVar(fn *FuncValue, name, usage string, options ...Option) {
 
 // Func defines a function flag with specified name, shortname, and usage string. It returns a pointer to a new FuncValue in which the function to call is stored.
 func (f *FlagSet) Func(name, usage string, value func(), options ...Option) *FuncValue {
-	fn := NewFunc(name, value)
+	fn := NewFunc("", value)
 	f.FuncVar(fn, name, usage, options...)
 	return fn
 }
 
 // FuncWithArg defines a function flag with specified name, shortname, and usage string. It returns a pointer to a new FuncValue in which the function to call is stored.
 func (f *FlagSet) FuncWithArg(name, usage string, value SetterFunc, options ...Option) *FuncValue {
-	fn := NewFuncWithArg(name, value)
+	fn := NewFuncWithArg("", value)
 	f.FuncVar(fn, name, usage, options...)
 	return fn
 }
