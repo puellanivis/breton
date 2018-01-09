0fa4d9dc8ee0478d794b56a386ebefb9efe62952
Cassondra Foesch <puellanivis@gmail.com>
2018-01-09 13:23:55 +0000

    lib/display/width: go vet says struct composite literals shouldn't be used unkeyed

--- lib/display/width/tables.go
+++ lib/display/width/tables.go
@@ -18,12 +18,12 @@ var ZeroWidth = rangetable.Merge(unicode.Cf, unicode.Mn, unicode.Me, AdditionalZ
 // DoubleWidth is a Unicode Range that can be safely assumed to be Wide, even if not EastAsian{Wide,Fullwidth}.
 var DoubleWidth = &unicode.RangeTable{
 	R32: []unicode.Range32{
-		{0x1F030, 0x1F061, 1}, // Domino Tiles (horizontal)
-		{0x1F100, 0x1F1FF, 1}, // Enclosed Alphanumeric Supplement
-		{0x1F200, 0x1F2FF, 1}, // Enclosed Ideographic Supplement
-		{0x1F300, 0x1F5FF, 1}, // Miscellaneous Symbols and Pictographs
-		{0x1F600, 0x1F64F, 1}, // Emoticons
-		{0x1F680, 0x1F6FF, 1}, // Transport and Map Symbols
-		{0x1F900, 0x1F9FF, 1}, // Supplemental Symbols and Pictographs
+		{Lo: 0x1F030, Hi: 0x1F061, Stride: 1}, // Domino Tiles (horizontal)
+		{Lo: 0x1F100, Hi: 0x1F1FF, Stride: 1}, // Enclosed Alphanumeric Supplement
+		{Lo: 0x1F200, Hi: 0x1F2FF, Stride: 1}, // Enclosed Ideographic Supplement
+		{Lo: 0x1F300, Hi: 0x1F5FF, Stride: 1}, // Miscellaneous Symbols and Pictographs
+		{Lo: 0x1F600, Hi: 0x1F64F, Stride: 1}, // Emoticons
+		{Lo: 0x1F680, Hi: 0x1F6FF, Stride: 1}, // Transport and Map Symbols
+		{Lo: 0x1F900, Hi: 0x1F9FF, Stride: 1}, // Supplemental Symbols and Pictographs
 	},
 }
