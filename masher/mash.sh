0ca50da32b611542703c08ba71554a122b620728
Cassondra Foesch <puellanivis@gmail.com>
2018-02-16 13:48:28 +0000

    update masher/mash.sh to build deb files if there is a debian/control file

--- masher/mash.sh
+++ masher/mash.sh
@@ -43,6 +43,13 @@ while [[ "$#" -gt 0 ]]; do
 			NOBUILD="true"
 		;;
 
+		--deb)
+			DEB="true"
+		;;
+		--nodeb)
+			DEB="false"
+		;;
+
 		--)
 			shift
 			break
@@ -66,11 +73,21 @@ if [[ $ESCAPE == "true" ]]; then
 	exit 1
 fi
 
+case "${LINUX}${DARWIN}${WINDOWS}" in
+	"")
+		[[ $DEB == "" ]] && LINUX="true"
+	;;
+	truetrue*)
+		TESTING="true"
+esac
+
+[[ "${LINUX}${DARWIN}${WINDOWS}" == "" ]] && NOCOMPILE="true"
+
 if which go > /dev/null 2>&1 ; then
         GO_VERSION="$(go version)"
         GO_VERSION=${GO_VERSION#go version go}
         GO_VERSION=${GO_VERSION%% *}
-        echo Building with Go Version: $GO_VERSION
+        [[ -z "$NOCOMPILE" ]] && echo Building with Go Version: $GO_VERSION
 fi
 
 if ! ls *.go > /dev/null 2>&1 ; then
@@ -118,14 +135,6 @@ if [[ -n $NOGOFILES ]]; then
 	exit 0
 fi
 
-case "${LINUX}${DARWIN}${WINDOWS}" in
-	"")
-		LINUX="true"
-	;;
-	truetrue*)
-		TESTING="true"
-esac
-
 if [[ ("$TESTING" == "true") && ("$NOTESTING" != "true") ]]; then
 	echo testing...
 	go test || exit 1
@@ -144,8 +153,10 @@ if [[ $PACKAGE != "main" ]]; then
 	exit 0
 fi
 
-echo getting dependencies...
-go get -v -d || exit 1
+if [[ $NOCOMPILE != "true" ]]; then
+	echo getting dependencies...
+	go get -v -d || exit 1
+fi
 
 if [[ "$NOBUILD" == "true" ]]; then
 	exit 0
@@ -169,11 +180,17 @@ fi
 
 touch /tmp/stuff > /dev/null 2>&1 || mount -t tmpfs -o rw,nodev,nosuid,size=1G /dev/null /tmp
 
+if [[ -r ./.gitignore ]]; then
+	grep -qFx -e "bin" ./.gitignore || echo "bin" >> ./.gitignore
+fi
+
 if [[ $LINUX == "true" ]]; then
 	OUT="bin/linux.x86_64"
 	echo Compiling ${OUT}/${PROJECT}
 	[ -d "$OUT" ] || mkdir -p $OUT || exit 1
 	GOOS=linux GOARCH=amd64 go build -o "${OUT}/${PROJECT}" "${GOFLAGS}" || exit 1
+
+	[[ "$DEB" != "false" ]] && DEB="true"
 fi
 
 if [[ $DARWIN == "true" ]]; then
@@ -188,6 +205,74 @@ if [[ $WINDOWS == "true" ]]; then
 	echo Compiling ${OUT}/${PROJECT}.exe
 	[ -d "$OUT" ] || mkdir -p $OUT || exit 1
 	GOOS=windows GOARCH=amd64 go build -o "${OUT}/${PROJECT}.exe" "${GOFLAGS}" || exit 1
+fi
+
+if [[ ( $DEB == "true" ) && ( -r debian/control ) && ( -x bin/linux.x86_64/${PROJECT} ) ]]; then
+	VERSION=${BUILDSTAMP}
+	BIN_VERSION="$(./bin/linux.x86_64/${PROJECT} --version)"
+	[[ -n $BIN_VERSION ]] && echo BIN_VERSION=\"$BIN_VERSION\"
+	case "${BIN_VERSION}" in
+	*\ v*)
+		VERSION=$(echo "${BIN_VERSION}" | cut -d" " -f2)
+		VERSION=${VERSION#v}
+	;;
+	esac
+	echo VERSION=${VERSION}
+	ARCH="amd64" # TODO(puellanivis): this shouldn’t be baked in, but it’s already baked in all over, already.
+
+	if [[ -r ./.gitignore ]]; then
+		grep -qFx -e "build" ./.gitignore || echo "build" >> ./.gitignore
+		grep -qFx -e "debian/changelog.gz" -e "*.gz" ./.gitignore || echo "debian/changelog.gz" >> ./.gitignore
+		grep -qFx -e "*.deb" ./.gitignore || echo "*.deb" >> ./.gitignore
+	fi
+
+	install -d build/DEBIAN
+	for f in conffiles control; do
+		if [[ -r "debian/$f" ]]; then
+			install -m644 debian/$f build/DEBIAN/
+			sed -i "s/@@PROJECT@@/${PROJECT}/" build/DEBIAN/$f
+			sed -i "s/@@VERSION@@/${VERSION}/" build/DEBIAN/$f
+			sed -i "s/@@ARCH@@/${ARCH}/" build/DEBIAN/$f
+		fi
+	done
+	for f in postinst postrm prerm; do
+		if [[ -r debian/$f ]]; then
+			install -m755 debian/$f build/DEBIAN/
+		fi
+	done
+
+	DEB_PACKAGE=$(grep "^Package: " build/DEBIAN/control | cut -d" " -f2 )
+
+	# install copyright and changelog documentation
+	install -d build/usr/share/doc/${DEB_PACKAGE}
+	install -m644 debian/copyright build/usr/share/doc/${DEB_PACKAGE}/
+
+	if [[ -r CHANGELOG ]]; then
+		# if CHANGELOG is newer than changelog.gz, then compress it and write it to changelog.gz
+		if [[ CHANGELOG -nt debian/changelog.gz ]]; then
+			gzip -9 -c CHANGELOG > debian/changelog.gz
+		fi
+	else
+		WHOAMI="nobody <nobody@example.com>"
+		[[ -r debian/whoami ]] && WHOAMI="$(cat debian/whoami)"
+
+		# if changelog.gz does not exist, then make it with a bare-minimum changelog gzip file.
+		printf "${DEB_PACKAGE} (${VERSION}) unstable; urgency=low\n\n  * No information.\n\n -- ${WHOAMI}  $(date -R)\n" | gzip -9 > debian/changelog.gz
+	fi
+	install -m644 debian/changelog.gz build/usr/share/doc/${DEB_PACKAGE}/changelog.Debian.gz
+
+	# install binary
+	install -d build/usr/bin
+	install -m755 bin/linux.x86_64/${PROJECT} build/usr/bin/${PROJECT}
+
+	if [[ ( -n ${DEB_PACKAGE} ) && ( -n ${VERSION} ) && ( -n ${ARCH} ) ]]; then
+		DEB_FILE="${DEB_PACKAGE}_${VERSION}_${ARCH}.deb"
+
+		# Final packaging
+		rm -f "${DEB_FILE}"
+		fakeroot dpkg-deb --build build .
+		lintian -X binaries "${DEB_FILE}"
+	fi
 fi
 
 echo Complete
