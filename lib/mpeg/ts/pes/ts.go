bdc5adf4489a4e9d031b2c606bee6915607c7280
Cassondra Foesch <puellanivis@gmail.com>
2018-02-20 01:24:48 +0000

    mpeg/ts/pes: timestamp support

---
+++ lib/mpeg/ts/pes/ts.go
@@ -0,0 +1,32 @@
+package pes
+
+import (
+)
+
+func Timestamp(ts uint64) *uint64 {
+	return &ts
+}
+
+func decodeTS(b []byte) *uint64 {
+	var ts uint64
+
+	ts = uint64(b[0] >> 1) & 0x07
+	ts = (ts << 8) | uint64(b[1])
+	ts = (ts << 7) | uint64((b[2] >> 1) & 0x7F)
+	ts = (ts << 8) | uint64(b[3])
+	ts = (ts << 7) | uint64((b[4] >> 1) & 0x7F)
+
+	return &ts
+}
+
+func encodeTS(ts uint64) []byte {
+	b := make([]byte, 5)
+
+	b[0] = byte((ts >> 29) & 0x0E) | 1
+	b[1] = byte((ts >> 22) & 0xFF)
+	b[2] = byte((ts >> 14) & 0xFE) | 1
+	b[3] = byte((ts >> 7) & 0xFF)
+	b[4] = byte((ts << 1) & 0xFE) | 1
+
+	return b
+}
---
+++ lib/mpeg/ts/pes/ts_test.go
@@ -0,0 +1,50 @@
+package pes
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTimestampEncoding(t *testing.T) {
+	var inputTS uint64 = 0
+	b := encodeTS(inputTS)
+	expected := []byte{ 0x01, 0x00, 0x01, 0x00, 0x01 }
+
+	if !reflect.DeepEqual(b, expected) {
+		t.Errorf("encodeTS(0x%09x): expected [% 2x] got [% 2x]", inputTS, expected, b)
+	}
+
+	if ts := decodeTS(b); *ts != inputTS {
+		t.Errorf("decodeTS·encodeTS: expected 0x%09x got 0x%09x", inputTS, *ts)
+	}
+
+	inputTS = 0x1FFFFFFFF
+	b = encodeTS(inputTS)
+	expected = []byte{ 0x0f, 0xff, 0xff, 0xff, 0xff }
+
+	if !reflect.DeepEqual(b, expected) {
+		t.Errorf("encodeTS(0x%09x): expected [% 2x] got [% 2x]", inputTS, expected, b)
+	}
+
+	if ts := decodeTS(b); *ts != inputTS {
+		t.Errorf("decodeTS·encodeTS: expected 0x%09x got 0x%09x", inputTS, *ts)
+	}
+
+	inputTS = 0x1F3B795D1
+	b = encodeTS(inputTS)
+	// a bbbbbbbb cccccccc dddddddd eeeeeeee
+	// 1 11110011 10110111 10010101 11010001
+	// 0000abb1 bbbbbbcc ccccccd1 ddddddde eeeeeee1
+	//     111  11001110 1101111  00101011 1010001
+	// 00001111 11001110 11011111 00101011 10100011
+	//    0   f    c   e    d   f    2   b    a   3
+	expected = []byte{ 0x0f, 0xce, 0xdf, 0x2b, 0xa3 }
+
+	if !reflect.DeepEqual(b, expected) {
+		t.Errorf("encodeTS(0x%09x): expected [% 2x] got [% 2x]", inputTS, expected, b)
+	}
+
+	if ts := decodeTS(b); *ts != inputTS {
+		t.Errorf("decodeTS·encodeTS: expected 0x%09x got 0x%09x", inputTS, *ts)
+	}
+}
