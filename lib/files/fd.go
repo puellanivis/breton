bc751b31c13ee089854946316f88a0ed8a48b61c
Cassondra Foesch <puellanivis@gmail.com>
2018-03-01 12:55:17 +0000

    lib/files: file scheme should use uri.Opaque if uri.Path is empty, fd scheme should use the same code

--- lib/files/fd.go
+++ lib/files/fd.go
@@ -13,45 +13,34 @@ func init() {
 	RegisterScheme(&descriptorHandler{}, "fd")
 }
 
-func filename(uri *url.URL) (uintptr, error) {
-	filename := uri.Opaque
-	if uri.Opaque == "" {
-		filename = uri.String()
-
-		if len(uri.Scheme)+3 < len(filename) {
-			filename = filename[len(uri.Scheme)+3:]
-		}
-	}
-
-	u, err := strconv.ParseUint(filename, 0, 64)
-	return uintptr(u), err
-}
-
 func (h *descriptorHandler) Open(ctx context.Context, uri *url.URL) (Reader, error) {
-	fd, err := filename(uri)
+	fd, err := strconv.ParseUint(filename(uri), 0, 64)
 	if err != nil {
 		return nil, err
 	}
 
-	return os.NewFile(fd, uri.String()), nil
+	return os.NewFile(uintptr(fd), uri.String()), nil
 }
 
 func (h *descriptorHandler) Create(ctx context.Context, uri *url.URL) (Writer, error) {
-	fd, err := filename(uri)
+	fd, err := strconv.ParseUint(filename(uri), 0, 64)
 	if err != nil {
 		return nil, err
 	}
 
-	return os.NewFile(fd, uri.String()), nil
+	return os.NewFile(uintptr(fd), uri.String()), nil
 }
 
 func (h *descriptorHandler) List(ctx context.Context, uri *url.URL) ([]os.FileInfo, error) {
-	fd, err := filename(uri)
+	fd, err := strconv.ParseUint(filename(uri), 0, 64)
 	if err != nil {
 		return nil, err
 	}
 
-	f := os.NewFile(fd, uri.String())
+	f, err := os.NewFile(uintptr(fd), uri.String()), nil
+	if err != nil {
+		return nil, err
+	}
 	defer f.Close()
 
 	return f.Readdir(0)
--- lib/files/local.go
+++ lib/files/local.go
@@ -15,19 +15,28 @@ func init() {
 	RegisterScheme(Local, "file")
 }
 
+func filename(uri *url.URL) string {
+	fname := uri.Path
+	if fname == "" {
+		fname = uri.Opaque
+	}
+
+	return fname
+}
+
 // Open opens up a local filesystem file specified in the uri.Path for reading.
 func (h *localFS) Open(ctx context.Context, uri *url.URL) (Reader, error) {
-	return os.Open(uri.Path)
+	return os.Open(filename(uri))
 }
 
 // Create opens up a local filesystem file specified in the uri.Path for writing. It will create a new one if it does not exist.
 func (h *localFS) Create(ctx context.Context, uri *url.URL) (Writer, error) {
-	return os.Create(uri.Path)
+	return os.Create(filename(uri))
 }
 
 // List returns the whole slice of os.FileInfos for a specific local filesystem at uri.Path.
 func (h *localFS) List(ctx context.Context, uri *url.URL) ([]os.FileInfo, error) {
-	f, err := os.Open(uri.Path)
+	f, err := os.Open(filename(uri))
 	if err != nil {
 		return nil, err
 	}
