52161743b582039bdfab8f02a14bc2737693c059
Cassondra Foesch <puellanivis@gmail.com>
2018-03-03 03:07:39 +0000

    mpeg/framer: encapsulate bufio.Scanner, and provide a DetectContentType function

--- lib/mpeg/framer/framer.go
+++ lib/mpeg/framer/framer.go
@@ -4,63 +4,113 @@ import (
 	"bufio"
 	"bytes"
 	"io"
+	"net/http"
 
 	"github.com/pkg/errors"
 )
 
-func splitterFunc() bufio.SplitFunc {
-	firstFrame := true
-	var frameDetect []byte
+const (
+	AAC  = "audio/x-aac"
+	MP3  = "audio/mp3"
+	H264 = "video/h264"
+)
 
-	return func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-		if firstFrame {
-			if len(data) < 4 {
-				return 0, nil, nil
-			}
+type Scanner struct {
+	mediaType   string
+	frameDetect []byte
 
-			switch {
-			// MPEG ADTS.
-			case data[0] == 0xFF && data[1]&0xF0 == 0xF0:
-				frameDetect = append([]byte{}, data[:2]...)
+	*bufio.Scanner
+}
 
-			// H264 byte stream.
-			case data[0] == 0x00 && data[1] == 0x00 && data[2] == 0x00 && data[3] == 0x01:
-				frameDetect = append([]byte{}, data[:4]...)
+func (s *Scanner) MediaType() string {
+	if len(s.frameDetect) <= 0 {
+		return "unknown/unknown"
+	}
 
-			default:
-				return 0, nil, errors.New("MPEG sync word missing")
-			}
+	return s.mediaType
+}
 
+func (s *Scanner) GetPCR() int64 {
+	return 0
+}
+
+var (
+	syncWordMP3 = []byte{0xFF, 0xFA}
+	syncWordAAC = []byte{0xFF, 0xF1}
+	syncWordNAL = []byte{0x00, 0x00, 0x00, 0x01}
+)
+
+func DetectContentType(data []byte) string {
+	switch {
+	// MPEG ADTS wrapped MP3
+	case bytes.Equal(data[:2], syncWordMP3):
+		return MP3
+
+	// MPEG ADTS wrapped AAC
+	case bytes.Equal(data[:2], syncWordAAC):
+		return AAC
+
+	// H264 byte stream.
+	case bytes.Equal(data[:4], syncWordNAL):
+		return H264
+	}
+
+	return http.DetectContentType(data)
+}
+
+func (s *Scanner) splitter(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if len(s.frameDetect) <= 0 {
+		if len(data) < 4 {
+			return 0, nil, nil
 		}
-		firstFrame = false
 
-		advance = 1
+		switch {
+		// MPEG ADTS wrapped MP3
+		case bytes.Equal(data[:2], syncWordMP3):
+			s.mediaType = MP3
+			s.frameDetect = append([]byte{}, data[:2]...)
 
-		if len(data) < 1 && atEOF {
-			return 0, nil, io.EOF
+		// MPEG ADTS wrapped AAC
+		case bytes.Equal(data[:2], syncWordAAC):
+			s.mediaType = AAC
+			s.frameDetect = append([]byte{}, data[:2]...)
+
+		// H264 byte stream.
+		case bytes.Equal(data[:4], syncWordNAL):
+			s.mediaType = H264
+			s.frameDetect = append([]byte{}, data[:4]...)
+
+		default:
+			return 0, nil, errors.New("unable to detect type")
 		}
+	}
 
-		for j := 0; ; advance++ {
-			j = bytes.Index(data[advance:], frameDetect)
-			if j < 0 {
-				if atEOF {
-					return len(data), data, nil
-				}
+	advance = 1
 
-				// need more data
-				return 0, nil, nil
+	if len(data) < 1 && atEOF {
+		return 0, nil, io.EOF
+	}
+
+	for j := 0; ; advance++ {
+		j = bytes.Index(data[advance:], s.frameDetect)
+		if j < 0 {
+			if atEOF {
+				return len(data), data, nil
 			}
 
-			advance += j
-			if frameDetect[0] != 0xFF || data[advance+1]&0xF0 == 0xF0 {
-				return advance, data[:advance], nil
-			}
+			// need more data
+			return 0, nil, nil
 		}
+
+		advance += j
+		return advance, data[:advance], nil
 	}
 }
 
-func NewScanner(r io.Reader) *bufio.Scanner {
-	s := bufio.NewScanner(r)
-	s.Split(splitterFunc())
+func NewScanner(r io.Reader) *Scanner {
+	s := &Scanner{
+		Scanner: bufio.NewScanner(r),
+	}
+	s.Split(s.splitter)
 	return s
 }
