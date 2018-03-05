99d64ab1f7b0b2e3c94c04e3351a645310557202
Cassondra Foesch <puellanivis@gmail.com>
2018-03-05 15:00:18 +0000

    mpeg/framer: account for MP3/AAC with/without crc protection

--- lib/mpeg/framer/framer.go
+++ lib/mpeg/framer/framer.go
@@ -35,8 +35,12 @@ func (s *Scanner) GetPCR() int64 {
 }
 
 var (
-	syncWordMP3 = []byte{0xFF, 0xFA}
+	syncWordMP3nocrc = []byte{0xFF, 0xFA}
+	syncWordMP3 = []byte{0xFF, 0xFB}
+
+	syncWordAACnocrc = []byte{0xFF, 0xF0}
 	syncWordAAC = []byte{0xFF, 0xF1}
+
 	syncWordNAL = []byte{0x00, 0x00, 0x00, 0x01}
 )
 
@@ -66,12 +70,12 @@ func (s *Scanner) splitter(data []byte, atEOF bool) (advance int, token []byte, 
 
 		switch {
 		// MPEG ADTS wrapped MP3
-		case bytes.Equal(data[:2], syncWordMP3):
+		case bytes.Equal(data[:2], syncWordMP3), bytes.Equal(data[:2], syncWordMP3nocrc):
 			s.mediaType = MP3
 			s.frameDetect = append([]byte{}, data[:2]...)
 
 		// MPEG ADTS wrapped AAC
-		case bytes.Equal(data[:2], syncWordAAC):
+		case bytes.Equal(data[:2], syncWordAAC), bytes.Equal(data[:2], syncWordAACnocrc):
 			s.mediaType = AAC
 			s.frameDetect = append([]byte{}, data[:2]...)
 
