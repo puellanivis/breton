b06fb694a3fcb3fc6588d586a84fdd0f918910af
Cassondra Foesch <puellanivis@gmail.com>
2018-07-17 17:55:16 +0000

    lib/metrics/internal/atomic: golint

--- lib/metrics/internal/atomic/counter.go
+++ lib/metrics/internal/atomic/counter.go
@@ -5,16 +5,20 @@ import (
 	"sync/atomic"
 )
 
+// Counter is a simple atomic monotonously-increasing counter.
 type Counter uint64
 
+// Inc increments the Counter by one (1).
 func (c *Counter) Inc() {
 	c.Add(1)
 }
 
+// Add increments the Counter by a given integer (must be non-negative).
 func (c *Counter) Add(delta uint64) {
 	atomic.AddUint64((*uint64)(c), delta)
 }
 
+// Get returns a momentary value of the Counter.
 func (c *Counter) Get() uint64 {
 	return atomic.LoadUint64((*uint64)(c))
 }
--- lib/metrics/internal/atomic/value.go
+++ lib/metrics/internal/atomic/value.go
@@ -4,28 +4,35 @@ import (
 	"sync/atomic"
 )
 
+// Value is a atomic fully variable integer Value.
 type Value int64
 
+// Inc increments the Value by one (1).
 func (v *Value) Inc() {
 	v.Add(1)
 }
 
+// Dec decrements the Value by one (1).
 func (v *Value) Dec() {
 	v.Add(-1)
 }
 
+// Add increments the Value by a given integer (may be negative).
 func (v *Value) Add(delta int64) {
 	atomic.AddInt64((*int64)(v), delta)
 }
 
+// Sub decrements the Value by a given integer (may be negative).
 func (v *Value) Sub(delta int64) {
 	v.Add(-delta)
 }
 
+// Set assigns a value into the Value, overriding any previous value.
 func (v *Value) Set(value int64) {
 	atomic.StoreInt64((*int64)(v), value)
 }
 
+// Get returns a momentary value of the Value.
 func (v *Value) Get() int64 {
 	return atomic.LoadInt64((*int64)(v))
 }
