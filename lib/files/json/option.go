50a0064d5c9d7f1c7f62cc8ab93bad62fbc518ca
Cassondra Foesch <puellanivis@gmail.com>
2020-07-27 11:55:38 +0000

    refactor json package

--- lib/files/json/option.go
+++ lib/files/json/option.go
@@ -1,55 +1,44 @@
 package json
 
+import (
+	"encoding/json"
+)
+
 type config struct {
+	*json.Encoder
+
 	prefix, indent string
 
-	escapeHTML, compact bool
+	compact bool
 }
 
-// An Option is a function that apply a specific option, then returns an Option function
-// that will revert the change applied.
-type Option func(*config) Option
+// An Option is a function that applies a specific option to an encoder config.
+type Option func(*config)
 
 // WithPrefix returns a function that directs Marshal to use the prefix string given.
 func WithPrefix(prefix string) Option {
-	return func(c *config) Option {
-		save := c.prefix
-
+	return func(c *config) {
 		c.prefix = prefix
-
-		return WithPrefix(save)
 	}
 }
 
 // WithIndent returns a function that directs Marshal to use the indenting string given.
 func WithIndent(indent string) Option {
-	return func(c *config) Option {
-		save := c.indent
-
+	return func(c *config) {
 		c.indent = indent
-
-		return WithIndent(save)
 	}
 }
 
 // EscapeHTML returns a function that directs Marshal to either enable or disable HTML escaping.
-func EscapeHTML(value bool) Option {
-	return func(c *config) Option {
-		save := c.escapeHTML
-
-		c.escapeHTML = value
-
-		return EscapeHTML(save)
+func EscapeHTML(on bool) Option {
+	return func(c *config) {
+		c.SetEscapeHTML(on)
 	}
 }
 
 // Compact returns a function that directs Marshal to use compact format.
 func Compact(value bool) Option {
-	return func(c *config) Option {
-		save := c.compact
-
+	return func(c *config) {
 		c.compact = value
-
-		return Compact(save)
 	}
 }
