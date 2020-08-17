72c7b1fd42db895787b8df171157f09729042d93
Cassondra Foesch <puellanivis@gmail.com>
2020-08-17 17:03:44 +0000

    change context keys to keyXYZ instead of xyzKey

--- lib/files/cachefiles/context.go
+++ lib/files/cachefiles/context.go
@@ -6,18 +6,18 @@ import (
 )
 
 type (
-	expireKey     struct{}
-	reentranceKey struct{}
+	keyExpiration struct{}
+	keyReentrance struct{}
 )
 
 // WithExpire returns a Context that includes information for the cache FileStore to expire buffers after the given timeout.
 func WithExpire(ctx context.Context, timeout time.Duration) context.Context {
-	return context.WithValue(ctx, expireKey{}, timeout)
+	return context.WithValue(ctx, keyExpiration{}, timeout)
 }
 
 // GetExpire returns the expiration timeout specified for the given Context.
 func GetExpire(ctx context.Context) (time.Duration, bool) {
-	timeout, ok := ctx.Value(expireKey{}).(time.Duration)
+	timeout, ok := ctx.Value(keyExpiration{}).(time.Duration)
 
 	return timeout, ok
 }
@@ -26,9 +26,9 @@ func GetExpire(ctx context.Context) (time.Duration, bool) {
 // a new sub-context with a reentrance key attached, along with a false bool,
 // or the same context input with a true bool.
 func isReentrant(ctx context.Context) (rctx context.Context, reentrant bool) {
-	if v := ctx.Value(reentranceKey{}); v != nil {
+	if v := ctx.Value(keyReentrance{}); v != nil {
 		return ctx, true
 	}
 
-	return context.WithValue(ctx, reentranceKey{}, struct{}{}), false
+	return context.WithValue(ctx, keyReentrance{}, struct{}{}), false
 }
