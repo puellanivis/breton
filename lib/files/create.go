757c7508472ef6abc61b47ba629be052f160bcae
Cassondra Foesch <puellanivis@gmail.com>
2020-08-18 17:41:46 +0000

    documentation changes and argument name changes

--- lib/files/create.go
+++ lib/files/create.go
@@ -7,12 +7,13 @@ import (
 	"path/filepath"
 )
 
-// Create creates a new file at the given filename, and
-// returns a files.Writer, which can be used to write contents to that filename.
+// Create returns a files.Writer, which can be used to write content to the resource at the given URL.
+//
+// If the given URL is a local filename, the file will be created, and truncated before this function returns.
 //
 // All errors and reversion functions returned by Option arguments are discarded.
-func Create(ctx context.Context, filename string, options ...Option) (Writer, error) {
-	f, err := create(ctx, filename)
+func Create(ctx context.Context, url string, options ...Option) (Writer, error) {
+	f, err := create(ctx, url)
 	if err != nil {
 		return nil, err
 	}
@@ -24,19 +25,19 @@ func Create(ctx context.Context, filename string, options ...Option) (Writer, er
 	return f, nil
 }
 
-func create(ctx context.Context, filename string) (Writer, error) {
-	switch filename {
+func create(ctx context.Context, resource string) (Writer, error) {
+	switch resource {
 	case "", "-", "/dev/stdout":
 		return os.Stdout, nil
 	case "/dev/stderr":
 		return os.Stderr, nil
 	}
 
-	if filepath.IsAbs(filename) {
-		return os.Create(filename)
+	if filepath.IsAbs(resource) {
+		return os.Create(resource)
 	}
 
-	if uri, err := url.Parse(filename); err == nil {
+	if uri, err := url.Parse(resource); err == nil {
 		uri = resolveFilename(ctx, uri)
 
 		if fs, ok := getFS(uri); ok {
@@ -44,5 +45,5 @@ func create(ctx context.Context, filename string) (Writer, error) {
 		}
 	}
 
-	return os.Create(filename)
+	return os.Create(resource)
 }
--- lib/files/open.go
+++ lib/files/open.go
@@ -8,12 +8,11 @@ import (
 	"path/filepath"
 )
 
-// Open opens the file at the given filename, and
-// returns a files.Reader, which will read the contents of that filename.
+// Open returns a files.Reader, which can be used to read content from the resource at the given URL.
 //
 // All errors and reversion functions returned by Option arguments are discarded.
-func Open(ctx context.Context, filename string, options ...Option) (Reader, error) {
-	f, err := open(ctx, filename)
+func Open(ctx context.Context, url string, options ...Option) (Reader, error) {
+	f, err := open(ctx, url)
 	if err != nil {
 		return nil, err
 	}
@@ -25,17 +24,17 @@ func Open(ctx context.Context, filename string, options ...Option) (Reader, erro
 	return f, nil
 }
 
-func open(ctx context.Context, filename string) (Reader, error) {
-	switch filename {
+func open(ctx context.Context, resource string) (Reader, error) {
+	switch resource {
 	case "", "-", "/dev/stdin":
 		return os.Stdin, nil
 	}
 
-	if filepath.IsAbs(filename) {
-		return os.Open(filename)
+	if filepath.IsAbs(resource) {
+		return os.Open(resource)
 	}
 
-	if uri, err := url.Parse(filename); err == nil {
+	if uri, err := url.Parse(resource); err == nil {
 		uri = resolveFilename(ctx, uri)
 
 		if fs, ok := getFS(uri); ok {
@@ -43,22 +42,26 @@ func open(ctx context.Context, filename string) (Reader, error) {
 		}
 	}
 
-	return os.Open(filename)
+	return os.Open(resource)
 }
 
-// ReadDir reads the directory at the given filename, and
+// ReadDir reads the directory or listing of the resource at the given URL, and
 // returns a slice of os.FileInfo, which describe the files contained in the directory.
-func ReadDir(ctx context.Context, filename string) ([]os.FileInfo, error) {
-	switch filename {
+func ReadDir(ctx context.Context, url string) ([]os.FileInfo, error) {
+	return readDir(ctx, url)
+}
+
+func readDir(ctx context.Context, resource string) ([]os.FileInfo, error) {
+	switch resource {
 	case "", "-", "/dev/stdin":
 		return os.Stdin.Readdir(0)
 	}
 
-	if filepath.IsAbs(filename) {
-		return ioutil.ReadDir(filename)
+	if filepath.IsAbs(resource) {
+		return ioutil.ReadDir(resource)
 	}
 
-	if uri, err := url.Parse(filename); err == nil {
+	if uri, err := url.Parse(resource); err == nil {
 		uri = resolveFilename(ctx, uri)
 
 		if fs, ok := getFS(uri); ok {
@@ -66,13 +69,13 @@ func ReadDir(ctx context.Context, filename string) ([]os.FileInfo, error) {
 		}
 	}
 
-	return ioutil.ReadDir(filename)
+	return ioutil.ReadDir(resource)
 }
 
-// List reads the directory at the given filename, and
+// List reads the directory or listing of the resource at the given URL, and
 // returns a slice of os.FileInfo, which describe the files contained in the directory.
 //
 // Depcrecated: Use `ReadDir`.
-func List(ctx context.Context, filename string) ([]os.FileInfo, error) {
-	return ReadDir(ctx, filename)
+func List(ctx context.Context, url string) ([]os.FileInfo, error) {
+	return readDir(ctx, url)
 }
