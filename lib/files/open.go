92d850ac7d6c622d066a65f743149a49cca88a13
Cassondra Foesch <puellanivis@gmail.com>
2020-08-18 17:21:07 +0000

    reformat ReadDir godoc

--- lib/files/open.go
+++ lib/files/open.go
@@ -3,9 +3,7 @@ package files
 import (
 	"context"
 	"io/ioutil"
-	"net/url"
 	"os"
-	"path/filepath"
 )
 
 // Open opens the file at the given filename, and
@@ -31,46 +29,40 @@ func open(ctx context.Context, filename string) (Reader, error) {
 		return os.Stdin, nil
 	}
 
-	if filepath.IsAbs(filename) {
+	uri := parsePath(ctx, filename)
+	if isPath(uri) {
 		return os.Open(filename)
 	}
 
-	if uri, err := url.Parse(filename); err == nil {
-		uri = resolveFilename(ctx, uri)
-
-		if fs, ok := getFS(uri); ok {
-			return fs.Open(ctx, uri)
-		}
+	if fs, ok := getFS(uri); ok {
+		return fs.Open(ctx, uri)
 	}
 
-	return os.Open(filename)
+	return nil, ErrNotSupported
 }
 
-// ReadDir reads the directory at the given filename, and returns a slice of os.FileInfo,
-// which describes all of the files contained in the directory.
+// ReadDir reads the directory at the given filename, and
+// returns a slice of os.FileInfo, which describe the files contained in the directory.
 func ReadDir(ctx context.Context, filename string) ([]os.FileInfo, error) {
 	switch filename {
 	case "", "-", "/dev/stdin":
 		return os.Stdin.Readdir(0)
 	}
 
-	if filepath.IsAbs(filename) {
+	uri := parsePath(ctx, filename)
+	if isPath(uri) {
 		return ioutil.ReadDir(filename)
 	}
 
-	if uri, err := url.Parse(filename); err == nil {
-		uri = resolveFilename(ctx, uri)
-
-		if fs, ok := getFS(uri); ok {
-			return fs.List(ctx, uri)
-		}
+	if fs, ok := getFS(uri); ok {
+		return fs.List(ctx, uri)
 	}
 
-	return ioutil.ReadDir(filename)
+	return nil, ErrNotSupported
 }
 
-// List reads the directory at the given filename, and returns a slice of os.FileInfo,
-// which describes all of the files contained in the directory.
+// List reads the directory at the given filename, and
+// returns a slice of os.FileInfo, which describe the files contained in the directory.
 //
 // Depcrecated: Use `ReadDir`.
 func List(ctx context.Context, filename string) ([]os.FileInfo, error) {
