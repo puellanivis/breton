f5d61fc58ed4b42498b1f814b3fcc42a694dfdd8
Cassondra Foesch <puellanivis@gmail.com>
2021-02-17 09:54:01 +0000

    Introduce AppName and AppVersion for just parts of the version string

--- lib/os/process/version.go
+++ lib/os/process/version.go
@@ -2,15 +2,34 @@ package process
 
 var versionString string
 
-// Version returns the version information populated during process.Init().
+// Version returns the full version information populated during process.Init().
+//
+// This function is intended to implement a `--version` flag, and thus returns `"cmdname version[-buildstamp]"`.
+// It does _not_ return just the `"version[-buildstamp]"` information, for that use `AppVersion()`.
 func Version() string {
 	return versionString
 }
 
-func buildVersion(cmdname, semver, buildstamp string) {
-	versionString = cmdname + " " + semver
+var (
+	appName    string
+	appVersion string
+)
 
+// AppName returns the command name populated during process.Init().
+func AppName() string {
+	return appName
+}
+
+// AppVersion returns only the version and buildstamp populated during process.Init().
+func AppVersion() string {
+	return appVersion
+}
+
+func buildVersion(cmdname, version, buildstamp string) {
+	appName, appVersion = cmdname, version
 	if buildstamp != "" {
-		versionString = versionString + "-" + buildstamp
+		appVersion += "-" + buildstamp
 	}
+
+	versionString = appName + " " + appVersion
 }
